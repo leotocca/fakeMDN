f4d38a6df9bd454665cb26dcf7107b40f7389166
Martin <quintana.martin1988@gmail.com>
2019-06-08 12:44:28 -0300

    delete my comments

--- app.js
+++ app.js
@@ -21,35 +21,19 @@ function fakeFilter(arr, callbackFunction) {
 
 function fakeForEach(arr, fn) {
   for (let element of arr) {
-    //function will be called in each of the array elements
     element = fn(element);
   }
-  //returns the new array
   return arr;
 }
 
-//console.log(fakeForEach(['tincho', 'peter', 'nico'], x => x + 'Medialunas'));
-//console.log(fakeForEach([1, 2, 3], multiplyBy2 => multiplyBy2 * 2));
-
-/*
-const array = [1, 2, 3, 4, 5];
-
-const even = function(element) {
-  // checks whether an element is even
-  return element % 2 === 0;
-}
-*/
 function fakeSome(arr, fn) {
   for (let element of arr) {
-    //If fn returns true with any of the array elements, iteration stops
     if (fn(element)) {
       return true;
     }
   }
-  //Iterates through the whole array and if it doesnt return true, it will be false.
   return false;
 }
-//console.log(fakeSome(array, even))
 
 function fakeMap(arr, fn) {
   const arrMaped = [];
