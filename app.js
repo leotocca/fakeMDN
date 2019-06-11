e9dd2e13735cc44b6f422076d9db1e7eaf8c6bd7
carigonz <caroliagonzalez794@gmail.com>
2019-06-11 20:53:23 -0300

    no se que le pasa a git

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-function fakeForEach(arr, fn) {
-  for (let i = 0; i < arr.length; i++) {
-    //function will be called in each of the array elements
-    arr[i] = fn(arr[i]);
+function fakeReduce(arr, callbackFunction) {
+  let accumulator = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    accumulator = callbackFunction(accumulator, arr[i]);
   }
-  //returns the new array
-  return arr;
+  return accumulator;
 }
 
-//console.log(fakeForEach(['tincho', 'peter', 'nico'], x => x + 'Medialunas'));
-//console.log(fakeForEach([1, 2, 3], multiplyBy2 => multiplyBy2 * 2));
+function fakeFilter(arr, callbackFunction) {
+  // filters the array using callbackFunction and returns filtered array
+  let newarr = [];
+  let j = 0; // j is the index of the new array I´m creating
+  for (let i = 0; i < arr.length; i++) {
+    if (callbackFunction(arr[i])) {
+      newarr[j] = arr[i];
+      j++;
+    }
+  }
+  return newarr;
+}
+
+function fakeForEach(arr, fn) {
+  for (let element of arr) {
+    fn(element);
+  }
+}
+
+function fakeSome(arr, fn) {
+  let atLeastOnePassesTheTest = false;
+  for (let element of arr) {
+    if (fn(element)) {
+      atLeastOnePassesTheTest = true;
+    }
+  }
+  return atLeastOnePassesTheTest;
+}
+
+function fakeMap(arr, fn) {
+  const arrMaped = [];
+  /* for (let i = 0; i < arr.length; i++) {
+    arrMaped.push(fn(arr[i]));
+  } */
+  fakeForEach(arr, function(arr) {
+    arrMaped.push(fn(arr));
+  });
+  return arrMaped;
+}
+//fakeMap([1,2,3], function(x){return x*2});
+
+function fakeIncludes(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+//fakeIncludes([1,2,3], "a");
