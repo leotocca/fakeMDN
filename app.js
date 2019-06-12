4333345bb4d3f9a57727d819a598f2722271932a
Miguel Lederkremer <mlederkremer@gmail.com>
2019-06-11 22:13:55 -0300

    modifiqué fakeFilter para que utilice FakeForEach e hice fakeSum

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
+arr = [1, 2 , 3, 4 ,5 , 6 , 7 ];
 
-
-function fakeReduce(arr, callbackFunction) {
-  let accumulator = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    accumulator = callbackFunction(accumulator, arr[i]);
-  }
-  return accumulator;
+function reductora (a,b) {
+  return (a+b);
 }
+console.log(arr);
+console.log(fakeSum(arr));
 
-function fakeFilter(arr, callbackFunction) {
-  // filters the array using callbackFunction and returns filtered array
-  let newarr = [];
-  let j = 0; // j is the index of the new array I´m creating
-  for (let i = 0; i < arr.length; i++) {
-    if (callbackFunction(arr[i])) {
-      newarr[j] = arr[i];
-      j++;
-    }
-  }
-  return newarr;
-}
 
 function fakeForEach(arr, fn) {
   for (let i = 0; i < arr.length; i++) {
@@ -29,6 +15,32 @@ function fakeForEach(arr, fn) {
   //returns the new array
   return arr;
 }
+
+function fakeReduce(arr, callbackFunction) {
+  let accumulator = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    accumulator = callbackFunction(accumulator, arr[i]);
+  }
+  return accumulator;
+}
+
+function fakeSum(arr) {
+  return fakeReduce(arr, (a, b) => a+b); 
+}
+
+function fakeFilter(arr, callbackFunction) {
+  // filters the array using callbackFunction and returns filtered array
+  let newarr = [];
+  let j = 0; // j is the index of the new array I´m creating
+  fakeForEach(arr,  elem => {
+    if (callbackFunction(elem)) {
+      newarr[j] = elem;
+      j++;
+    }
+  })
+  return newarr;
+}
+
 
 //console.log(fakeForEach(['tincho', 'peter', 'nico'], x => x + 'Medialunas'));
 //console.log(fakeForEach([1, 2, 3], multiplyBy2 => multiplyBy2 * 2));
