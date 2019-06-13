55a2c00a3abd576b261b6e48d245f57f282c51b4
Leonardo Toccaceli <leotoccaceli@gmail.com>
2019-06-13 12:16:25 -0300

    Added fakeUnion() and fakeIntersection() to app,js. Also added fakeUnion.html and fakeIntersection.html

--- app.js
+++ app.js
@@ -1,69 +1,96 @@
 function fakeReduce(arr, callbackFunction) {
-  let accumulator = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    accumulator = callbackFunction(accumulator, arr[i]);
-  }
-  return accumulator;
+	let accumulator = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		accumulator = callbackFunction(accumulator, arr[i]);
+	}
+	return accumulator;
 }
 
 function fakeSum(arr) {
-  return fakeReduce(arr, (a, b) => a + b);
+	return fakeReduce(arr, (a, b) => a + b);
 }
 
 function fakeFilter(arr, callbackFunction) {
-  let newarr = [];
-  let j = 0; // j is the index of the new array I´m creating
-  fakeForEach(arr, elem => {
-    if (callbackFunction(elem)) {
-      newarr[j] = elem;
-      j++;
-    }
-  });
-  return newarr;
+	let newarr = [];
+	let j = 0; // j is the index of the new array I´m creating
+	fakeForEach(arr, elem => {
+		if (callbackFunction(elem)) {
+			newarr[j] = elem;
+			j++;
+		}
+	});
+	return newarr;
 }
 
 function fakeForEach(arr, fn) {
-  for (let element of arr) {
-    fn(element);
-  }
+	for (let element of arr) {
+		fn(element);
+	}
 }
 
 function fakeSome(arr, fn) {
-  let atLeastOnePassesTheTest = false;
-  for (let element of arr) {
-    if (fn(element)) {
-      atLeastOnePassesTheTest = true;
-    }
-  }
-  return atLeastOnePassesTheTest;
+	let atLeastOnePassesTheTest = false;
+	for (let element of arr) {
+		if (fn(element)) {
+			atLeastOnePassesTheTest = true;
+		}
+	}
+	return atLeastOnePassesTheTest;
 }
 
 function fakeMap(arr, fn) {
-  const arrMaped = [];
-  fakeForEach(arr, function(arr) {
-    arrMaped.push(fn(arr));
-  });
-  return arrMaped;
+	const arrMaped = [];
+	fakeForEach(arr, function(arr) {
+		arrMaped.push(fn(arr));
+	});
+	return arrMaped;
 }
 
 function fakeIncludes(arr, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === value) {
-      return true;
-    }
-  }
-  return false;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === value) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function fakeIndexOf(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return array[i];
-    }
-  }
-  return -1;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === value) {
+			return array[i];
+		}
+	}
+	return -1;
 }
 
 function fakeIncludes(arr, value) {
-  return fakeIndexOf(arr, value) === -1 ? false : true;
+	return fakeIndexOf(arr, value) === -1 ? false : true;
+}
+
+function fakeIntersection(arr1, arr2) {
+	let newArr = [];
+	for (const i of arr1) {
+		for (const j of arr2) {
+			if (i === j) {
+				newArr.push(j);
+			}
+		}
+	}
+	return newArr;
+}
+
+function fakeUnion(arr1, ...arr2) {
+	const newArr = [];
+	for (const i of arr1) {
+		newArr.push(i);
+	}
+	fakeForEach(arr2, function(i) {
+		fakeForEach(i, function(j) {
+			if (!newArr.includes(j)) {
+				newArr.push(j);
+			}
+		});
+	});
+	return newArr;
 }
