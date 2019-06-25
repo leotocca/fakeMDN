7885042751b196a2e77820e5ad26ee983cf11b2f
carigonz <caroliagonzalez794@gmail.com>
2019-06-25 10:15:42 -0300

    desarrollo de fakeConcat html + js

--- app.js
+++ app.js
@@ -1,124 +1,141 @@
+function fakeConcat(array1, array2) {
+	let arrayConcated = [];
+	Array.isArray(array1)
+		? (arrayConcated = array1.slice())
+		: array1 !== undefined
+		? arrayConcated.push(array1)
+		: (newArray = []);
+	if (Array.isArray(array2)) {
+		for (const elem of array2) {
+			arrayConcated.push(elem);
+		}
+	} else if (array2 !== undefined) {
+		arrayConcated.push(array2);
+	}
+	return arrayConcated;
+}
+
 function fakeMin(arr) {
-  if (arr.length === 0) return undefined;
-  return fakeReduce(arr, (acc, value) => (value < acc ? value : acc));
+	if (arr.length === 0) return undefined;
+	return fakeReduce(arr, (acc, value) => (value < acc ? value : acc));
 }
 
 function fakeMax(arr) {
-  if (arr.length === 0) return undefined;
-  return fakeReduce(arr, (acc, value) => (value > acc ? value : acc));
+	if (arr.length === 0) return undefined;
+	return fakeReduce(arr, (acc, value) => (value > acc ? value : acc));
 }
 
 function fakeReduce(arr, callbackFunction, initialValue) {
-  let accumulator = arr[0];
-  let begin = 1;
-  if (initialValue !== undefined) {
-    accumulator = initialValue;
-    begin = 0;
-  }
-  for (const elem of arr) {
-    accumulator = callbackFunction(accumulator, elem);
-  }
-  return accumulator;
+	let accumulator = arr[0];
+	let begin = 1;
+	if (initialValue !== undefined) {
+		accumulator = initialValue;
+		begin = 0;
+	}
+	for (const elem of arr) {
+		accumulator = callbackFunction(accumulator, elem);
+	}
+	return accumulator;
 }
 
 function fakeSum(array) {
-  return fakeReduce(array, (a, b) => a + b);
+	return fakeReduce(array, (a, b) => a + b);
 }
 
 function fakeFilter(array, callbackFunction) {
-  const arrFiltered = [];
-  fakeForEach(array, elem => {
-    if (callbackFunction(elem)) {
-      arrFiltered.push(elem);
-    }
-  });
-  return arrFiltered;
+	const arrFiltered = [];
+	fakeForEach(array, elem => {
+		if (callbackFunction(elem)) {
+			arrFiltered.push(elem);
+		}
+	});
+	return arrFiltered;
 }
 
 function fakeForEach(array, callbackFunction) {
-  for (const element of array) {
-    callbackFunction(element);
-  }
+	for (const element of array) {
+		callbackFunction(element);
+	}
 }
 
 function fakeSome(array, callbackFunction) {
-  for (const element of array) {
-    if (callbackFunction(element)) {
-      return true;
-    }
-  }
-  return false;
+	for (const element of array) {
+		if (callbackFunction(element)) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function fakeEvery(array, callbackFunction) {
-  fakeForEach(array, element => {
-    if (!callbackFunction(element)) {
-      return false;
-    }
-  });
-  return true;
+	fakeForEach(array, element => {
+		if (!callbackFunction(element)) {
+			return false;
+		}
+	});
+	return true;
 }
 
 function fakeFind(array, callbackFunction) {
-  for (const elem of array) {
-    if (callbackFunction(elem)) {
-      return elem;
-    }
-  }
-  return undefined;
+	for (const elem of array) {
+		if (callbackFunction(elem)) {
+			return elem;
+		}
+	}
+	return undefined;
 }
 
 function fakeMap(array, callbackFunction) {
-  const arrMaped = [];
-  fakeForEach(array, function(array) {
-    arrMaped.push(callbackFunction(array));
-  });
-  return arrMaped;
+	const arrMaped = [];
+	fakeForEach(array, function(array) {
+		arrMaped.push(callbackFunction(array));
+	});
+	return arrMaped;
 }
 
 function fakeIndexOf(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return i;
-    }
-  }
-  return -1;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === value) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 function fakeIndexOfRecursive(array, value) {
-  if (!array.length) return -1;
-  return array.pop() === value
-    ? array.length
-    : fakeIndexOfRecursive(array, value);
+	if (!array.length) return -1;
+	return array.pop() === value
+		? array.length
+		: fakeIndexOfRecursive(array, value);
 }
 
 function fakeIncludes(array, value) {
-  return fakeIndexOf(array, value) !== -1;
+	return fakeIndexOf(array, value) !== -1;
 }
 
 function fakeIntersection(array1, array2) {
-  const arrIntersection = [];
-  for (const i of array1) {
-    for (const j of array2) {
-      if (i === j) {
-        arrIntersection.push(j);
-      }
-    }
-  }
-  return arrIntersection;
+	const arrIntersection = [];
+	for (const i of array1) {
+		for (const j of array2) {
+			if (i === j) {
+				arrIntersection.push(j);
+			}
+		}
+	}
+	return arrIntersection;
 }
 
 function fakeUnion(arr1, ...arr2) {
-  const arrUnion = [];
-  for (const i of arr1) {
-    arrUnion.push(i);
-  }
-  fakeForEach(arr2, function(i) {
-    fakeForEach(i, function(j) {
-      if (!fakeIncludes(arrUnion, j)) {
-        arrUnion.push(j);
-      }
-    });
-  });
-  return arrUnion;
+	const arrUnion = [];
+	for (const i of arr1) {
+		arrUnion.push(i);
+	}
+	fakeForEach(arr2, function(i) {
+		fakeForEach(i, function(j) {
+			if (!fakeIncludes(arrUnion, j)) {
+				arrUnion.push(j);
+			}
+		});
+	});
+	return arrUnion;
 }
